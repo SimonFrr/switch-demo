43c9aa024da280986608ccb75b2759ca7c8e5322
SimonFrr <sim.frr@gmail.com>
2017-07-19 16:33:34 +0200

    Fixed, more concise PlanDisplayer

--- src/components/checkout/Review.js
+++ src/components/checkout/Review.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { compose } from 'redux';
-import { connect } from 'react-redux';
 import {
   Panel,
   Button
@@ -44,7 +43,6 @@ const Review = ({ onCheckoutSubmit, planName, planPrice }) => (
 );
 
 const ConnectedReview = compose(
-  connect(),
   PlanDisplayer
 )(Review);
 
--- src/components/generic/PlanDisplayer.js
+++ src/components/generic/PlanDisplayer.js
@@ -1,14 +1,9 @@
-import React from 'react';
 import { connect } from 'react-redux';
 import { getPlanName, getPlanPrice } from '../../reducers/plan';
-
-const HOC = ComponentToWrap => ({ planName, planPrice }) => {
-  return <ComponentToWrap planName={planName} planPrice={planPrice}/>
-};
 
 const mapStateToProps = state => ({
   planName: getPlanName(state),
   planPrice: getPlanPrice(state)
 });
 
-export default ComponentToWrap => connect(mapStateToProps)(HOC(ComponentToWrap));
+export default ComponentToWrap => connect(mapStateToProps)(ComponentToWrap);
