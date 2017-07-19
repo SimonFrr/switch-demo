5638bc24f7bf4db8e025e4051d481ba001e31808
SimonFrr <sim.frr@gmail.com>
2017-07-19 11:06:38 +0200

    lint

--- src/actions/queryParams.js
+++ src/actions/queryParams.js
@@ -1,7 +1,7 @@
 // https://davidwalsh.name/query-string-javascript
 // We use this instead of URLSearchParams for maximum compatibility
 function getUrlParameter(name) {
-  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  name = name.replace(/[[]/, '\\[').replace(/[\]]/, '\\]');
   var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
   var results = regex.exec(window.location.search);
   return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
